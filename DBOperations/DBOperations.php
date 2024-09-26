f11eb430e8c3fa0a9fa35e71f1f571f33231a189
atal-tripledots <135303345+atal-tripledots@users.noreply.github.com>
2024-09-26 16:48:45 +0530

    V

--- DBOperations/DBOperations.php
+++ DBOperations/DBOperations.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /*
 Name : DBOperations
 Description : DBOperations for Codeigniter 3
-Version : v0.12
+Version : v0.13
 */
 
 class DBOperations {
